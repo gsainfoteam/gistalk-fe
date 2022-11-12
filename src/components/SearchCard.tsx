2548749ccfce2e67c4af5b23a325becf6c92176b
Crown3 <strikedown7@gmail.com>
2022-11-12 19:47:59 +0900

    Search 페이지 완료

--- src/components/SearchCard.tsx
+++ src/components/SearchCard.tsx
@@ -1,37 +1,98 @@
 import styled from "styled-components";
+import { theme } from "../style/theme";
+import TempIcon from "./TempIcon";
 
-const SearchCardRow = styled.div`
+interface IProps {
+  subjectCode: string;
+  professorName: string;
+  subjectName: string;
+  subjectScore: string;
+}
+
+const SearchCardWrap = styled.div`
+  width: 100%;
+  margin-bottom: 15px;
   display: flex;
-  justify-content: space-between;
-  padding: 0 1em;
+  align-items: center;
 `;
 
-// const HighLightBox = styled.div`
-//   width: 90px;
-//   height: 90px;
-//   background-color: pink;
-//   transform: rotate(45deg);
-// `;
-
-const Card = styled.div`
-  background-color: gray;
-  width: 335px;
-  height: 62px;
-  border-radius: 5px;
-  background: linear-gradient(65deg, gray 70%, pink 30%);
+const CardContentWrap = styled.div<{ color: string }>`
+  padding-left: 10px;
+  color: ${(props) => props.color};
+  text-align: left;
+  text-overflow: ellipsis;
+  white-space: nowrap; // 개행 방지
+  overflow: hidden; // 넘치는 문자열 숨김
+  width: calc(
+    100% - 50px - 65px
+  ); //100%에서 TempIcon width, AverageScoreWrap width 뺀 값
+  p {
+    font-family: NSRegular;
+    font-size: 14px;
+    position: relative;
+    top: 1px;
+  }
+  span {
+    font-family: NSMedium;
+    text-overflow: ellipsis;
+  }
+  div {
+    font-family: NSBold;
+    font-size: 19px;
+    position: relative;
+    top: -1px;
+    white-space: nowrap; // 개행 방지
+    overflow: hidden; // 넘치는 문자열 숨김
+    text-overflow: ellipsis;
+  }
 `;
 
-export default function SearchCard() {
+const AverageScoreWrap = styled.div<{ color: string }>`
+  text-align: center;
+  width: 55px;
+  color: ${(props) => props.color};
+  font-family: NSBold;
+  p {
+    font-size: 13px;
+    position: relative;
+    top: 4px;
+  }
+  span {
+    font-size: 13px;
+  }
+  div {
+    font-size: 25px;
+    position: relative;
+    top: -4px;
+  }
+`;
+
+/** 검색했을 때 뜨는 강의평가를 표시하는 카드 */
+export default function SearchCard({
+  subjectCode,
+  professorName,
+  subjectName,
+  subjectScore,
+}: IProps) {
+  const Division = subjectCode.slice(0, 2);
+
   return (
-    <Card>
-      <SearchCardRow>
-        <div>EC2001-01 | 박창수</div>
-        <div>3학점</div>
-      </SearchCardRow>
-      <SearchCardRow>
-        <div>회로이론</div>
-        <div>4.5/5</div>
-      </SearchCardRow>
-    </Card>
+    <SearchCardWrap>
+      <TempIcon text={Division} color={"#FFCF23"}></TempIcon>
+      <CardContentWrap color={theme.primaryText}>
+        <p>
+          {subjectCode}&nbsp;&nbsp;|&nbsp;&nbsp; {/* 과목 코드 */}
+          <span>{professorName}</span> {/* 교수 이름 */}
+        </p>
+        <div>{subjectName}</div> {/* 과목 이름 */}
+      </CardContentWrap>
+      <AverageScoreWrap color={theme.primaryText}>
+        <p>평균점수</p>
+        <div>
+          {subjectScore}
+          <span>/5</span>
+        </div>
+      </AverageScoreWrap>
+    </SearchCardWrap>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import GlobalStyle from "./fonts/global";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./style/theme";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Search from "../src/components/Search";
@@ -9,10 +11,12 @@ import Search from "../src/components/Search";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Search />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Search />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
