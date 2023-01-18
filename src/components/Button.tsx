b601144b1f654f560b7ac8db8db4b5b46c271ddd
seolyeongbae <peixueying@gmail.com>
2023-01-18 21:18:24 +0900

    feat: title 디자인 완성

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,9 +4,9 @@ import { IButton } from "../Interfaces/interfaces";
 const StyledButton = styled.button<{ color: string; background: string }>`
   color: ${(props) => props.color || "gray"};
   background: ${(props) => props.background || "white"};
-  font-size: 1em;
+  font-size: 14px;
   margin: 1em;
-  padding: 0.25em 1em;
+  padding: 0.25em 0.5em;
   border: 2px solid ${(props) => props.background || "white"};
   border-radius: 3px;
 `;
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -3,6 +3,7 @@ import { theme } from "@/style/theme";
 import Header from "@/components/Header";
 import styled from "styled-components";
 import Button from "@/components/Button";
+import QuestionMark_svg from "../assets/svgs/circledQuestionMark.svg";
 
 const TitleWrap = styled.div<{ color: string }>`
   width: 87vw;
@@ -30,22 +31,63 @@ const MyReviewsText = styled(theme.universalComponent.DivTextContainer)`
   line-height: 42px;
 `;
 
-const Point = styled(theme.universalComponent.DivTextContainer)`
-  color: red;
+const Point = styled.div`
+  display: flex;
+  color: "gray";
+  font: 12px;
+  font-weight: bold;
 `;
 
+const PointValue = styled.div`
+  display: flex;
+  color: ${theme.colors.primary};
+  font: 12px;
+`;
+
+const HelperIcon = styled(theme.universalComponent.SvgIcon)`
+  margin-left: 4px;
+`;
+
+const Semester = styled(theme.universalComponent.DivTextContainer)`
+  border-bottom: ${(props) => props.color} 1.5px solid;
+  display: flex;
+  font-family: NSBold;
+  width: 40vw;
+  border-radius: 0;
+  padding-bottom: 0.5em;
+`;
+
+const Subject = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const SubjectName = styled(theme.universalComponent.DivTextContainer)``;
+const ProfessorName = styled(theme.universalComponent.DivTextContainer)``;
+const ArrowIcon = styled.div``;
 /** Radar Chart에 들어갈 임시 데이터 */
 
+const ContentWrap = styled.div`
+  width: 87vw;
+  margin: 10px auto 0 auto;
+`;
+
 export default function ProfilePage() {
+  const USER_NAME = "HongGilDong";
+  const POINT_VALUE = 54;
   return (
     <>
-      <Header text={"MY"}></Header>
+      <Header text={"MY"} />
       <TitleWrap color={theme.colors.grayStroke}>
         <div>
           <SubjectTitle fontSize={16} color={theme.colors.primaryText}>
-            ID: HongGilDong
+            ID: {USER_NAME}
           </SubjectTitle>
-          <Point>54P</Point>
+          <Point>
+            <PointValue>{POINT_VALUE}</PointValue>
+            <div>P</div>
+            <HelperIcon src={QuestionMark_svg} size={16} />
+          </Point>
         </div>
 
         <Button
@@ -55,12 +97,23 @@ export default function ProfilePage() {
           background={theme.colors.primary}
         />
       </TitleWrap>
-
-      <>
+      <ContentWrap>
         <MyReviewsText fontSize={16} color={theme.colors.primaryText}>
           · 내가 쓴 강의평 ·
         </MyReviewsText>
-      </>
+        <Semester fontSize={12} color={theme.colors.primary}>
+          2022년 1학기
+        </Semester>
+        <Subject>
+          <SubjectName fontSize={16} color={theme.colors.primaryText}>
+            컴퓨터 프로그래밍
+          </SubjectName>
+          <ProfessorName fontSize={16} color={theme.colors.primaryText}>
+            suman pandey
+          </ProfessorName>
+          <ArrowIcon> &gt; </ArrowIcon>
+        </Subject>
+      </ContentWrap>
     </>
   );
 }
