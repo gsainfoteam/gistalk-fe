6f8752dc60db65a060f39d235993c105e4810a8a
inthree3 <inseon_ug@gm.gist.ac.kr>
2023-01-29 23:10:38 +0900

    feat: Content 기능 구현
    하이라이트, 줄바꿈 가능

--- src/components/DescriptionModal.tsx
+++ src/components/DescriptionModal.tsx
@@ -21,24 +21,37 @@ const SubTitle = styled(theme.universalComponent.DivTextContainer)``;
 const HighLight = styled.span`
   color: ${theme.colors.primary};
 `;
-const ContentBox = styled(theme.universalComponent.DivTextContainer)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 11px 8vw 34px 8vw;
-  white-space: pre-wrap;
-`;
-const Content = ({ children }: { children: ReactNode }): JSX.Element => {
+
+const Content = ({
+  children,
+  fontSize,
+  color,
+}: {
+  children: ReactNode;
+  fontSize: number;
+  color: string;
+}) => {
   const content = String(children);
+  const ContentBox = styled(theme.universalComponent.DivTextContainer)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 11px 8vw 34px 8vw;
+    white-space: pre-wrap;
+  `;
   return (
-    <>
-      {content.split("\\n").map((text) => (
-        <>
-          {text}
+    <ContentBox fontSize={fontSize} color={color}>
+      {content.split("\\n").map((text, index) => (
+        <p>
+          {text
+            .split("|")
+            .map((text, subIndex) =>
+              subIndex % 2 == 1 ? <HighLight>{text}</HighLight> : <>{text}</>
+            )}
           <br />
-        </>
+        </p>
       ))}
-    </>
+    </ContentBox>
   );
 };
 
@@ -55,14 +68,13 @@ export default function DescriptionModal({ isOpen, setOpen }: IProps) {
             <SubTitle fontSize={16} color={theme.colors.primaryText}>
               <HighLight>지스톡 포인트</HighLight>란?
             </SubTitle>
-            <ContentBox fontSize={14} color={theme.colors.primaryText}>
-              <Content>
-                강의 후기를 작성하시면 포인트를 지급하며, \n 세부 강의평가 열람
-                시 차감됩니다. \n \n 세부 강의평가에서는 \n 시험 유형 / 과제
-                유형 / 학점 잘 받는 팁과 \n 같은 실질적인 수강 꿀팁들을 만나볼
-                수 있습니다.
-              </Content>
-            </ContentBox>
+
+            <Content fontSize={14} color={theme.colors.primaryText}>
+              |강의 후기를 작성|하시면 포인트를 지급하며, \n |세부 강의평가
+              열람| 시 차감됩니다. \n \n 세부 강의평가에서는 \n |시험 유형 /
+              과제 유형 / 학점 잘 받는 팁|과 \n 같은 실질적인 수강 꿀팁들을
+              만나볼 수 있습니다.
+            </Content>
           </Wrap>
         </Sheet.Content>
       </Sheet.Container>
