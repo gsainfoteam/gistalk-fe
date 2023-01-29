4f4893bd40de02b3a37f86a25601765a29b2dca9
inthree3 <inseon_ug@gm.gist.ac.kr>
2023-01-29 17:37:46 +0900

    feat: \n 인식해 줄바꿈하는 Content 컴포넌트 추가

--- src/components/DescriptionModal.tsx
+++ src/components/DescriptionModal.tsx
@@ -1,7 +1,8 @@
 import { theme } from "../style/theme";
 import styled from "styled-components";
 import Sheet from "react-modal-sheet";
-import React, { useState } from "react";
+import React, { ReactNode, useState } from "react";
+import { elements } from "chart.js";
 
 interface IProps {
   isOpen: boolean;
@@ -17,18 +18,29 @@ const Wrap = styled.div`
 `;
 const Title = styled(theme.universalComponent.DivTextContainer)``;
 const SubTitle = styled(theme.universalComponent.DivTextContainer)``;
-const HighLight = styled.text`
+const HighLight = styled.span`
   color: ${theme.colors.primary};
 `;
-const ContentBox = styled(theme.universalComponent.DivTextContainer)<{
-  content: string;
-}>`
+const ContentBox = styled(theme.universalComponent.DivTextContainer)`
   display: flex;
   flex-direction: column;
   align-items: center;
   margin: 11px 8vw 34px 8vw;
   white-space: pre-wrap;
 `;
+const Content = ({ children }: { children: ReactNode }): JSX.Element => {
+  const content = String(children);
+  return (
+    <>
+      {content.split("\\n").map((text) => (
+        <>
+          {text}
+          <br />
+        </>
+      ))}
+    </>
+  );
+};
 
 export default function DescriptionModal({ isOpen, setOpen }: IProps) {
   return (
@@ -44,16 +56,12 @@ export default function DescriptionModal({ isOpen, setOpen }: IProps) {
               <HighLight>지스톡 포인트</HighLight>란?
             </SubTitle>
             <ContentBox fontSize={14} color={theme.colors.primaryText}>
-              <p>
-                <HighLight>강의 후기를 작성하시면 포인트를 지급</HighLight>하며,
-              </p>
-              <p>
-                <HighLight>세부 강의평가 열람 시 차감</HighLight>됩니다.
-              </p>
-              <p></p>
-              <p>세부 강의평가에서는</p>
-              <HighLight>시험 유형 / 과제 유형 / 학점 잘 받는 팁</HighLight> 과
-              같은 실질적인 수강 꿀팁들을 만나볼 수 있습니다.
+              <Content>
+                강의 후기를 작성하시면 포인트를 지급하며, \n 세부 강의평가 열람
+                시 차감됩니다. \n \n 세부 강의평가에서는 \n 시험 유형 / 과제
+                유형 / 학점 잘 받는 팁과 \n 같은 실질적인 수강 꿀팁들을 만나볼
+                수 있습니다.
+              </Content>
             </ContentBox>
           </Wrap>
         </Sheet.Content>
