da7e42f080c65cbbad91da8dea6d3e33920c9dee
Crown3 <strikedown7@gmail.com>
2023-01-30 17:13:04 +0900

    refactor:Aharoni 글꼴의 로딩이 매우 오래 걸려서 woff2 형식으로 변환 및 수정

--- src/fonts/global.ts
+++ src/fonts/global.ts
@@ -2,6 +2,7 @@ import { createGlobalStyle } from "styled-components";
 import NSBold from "./noto-sans-kr-bold.woff2";
 import NSMedium from "./noto-sans-kr-medium.woff2";
 import NSRegular from "./noto-sans-kr-regular.woff2";
+import Aharoni from "./Aharoni.woff2";
 
 export default createGlobalStyle`
     @font-face {
@@ -17,12 +18,7 @@ export default createGlobalStyle`
         src: url(${NSMedium}) format('woff2');
     }
     @font-face {
-      font-family: "Aharoni"; 
-      src: url("//db.onlinewebfonts.com/t/22db60d19480ba0274c9eb6ba877ea9b.eot"); 
-      src: url("//db.onlinewebfonts.com/t/22db60d19480ba0274c9eb6ba877ea9b.eot?#iefix") 
-      format("embedded-opentype"), url("//db.onlinewebfonts.com/t/22db60d19480ba0274c9eb6ba877ea9b.woff2") 
-      format("woff2"), url("//db.onlinewebfonts.com/t/22db60d19480ba0274c9eb6ba877ea9b.woff") format("woff"), 
-      url("//db.onlinewebfonts.com/t/22db60d19480ba0274c9eb6ba877ea9b.ttf") format("truetype"), 
-      url("//db.onlinewebfonts.com/t/22db60d19480ba0274c9eb6ba877ea9b.svg#Aharoni") format("svg"); 
+      font-family: "Aharoni";
+      src: url(${Aharoni}) format('woff2');
     }
 `;
