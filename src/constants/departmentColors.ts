da985a349aa83a2d15027eb0392da0585820ecfa
yeong <80435616+SeolyeongBae@users.noreply.github.com>
2024-08-10 20:49:04 +0900

    fix: delete  margin

---
+++ src/constants/departmentColors.ts
@@ -0,0 +1,8 @@
+import { theme } from "@/style/theme";
+
+export const departmentColors = {
+  major: theme.colors.inputBg,
+  minor: theme.colors.inputBg,
+  underGraduate: "#00b102",
+  default: "#E0E0E0",
+};
