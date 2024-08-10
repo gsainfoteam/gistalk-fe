178771eeb30991fa072c2a12ff9606043e961f45
yeong <80435616+SeolyeongBae@users.noreply.github.com>
2024-08-10 21:14:16 +0900

    feat: 색상 수정, 아이콘 나머지 추가

--- src/assets/svgs/departmentIcons/departmentIcons.ts
+++ src/assets/svgs/departmentIcons/departmentIcons.ts
@@ -3,11 +3,22 @@ import CH from "./CH.svg";
 import EC from "./EC.svg";
 import EV from "./EV.svg";
 import GS from "./GS.svg";
+import MC from "./MC.svg";
 import MA from "./MA.svg";
-import ME from "./ME.svg";
 import MM from "./MM.svg";
 import PS from "./PS.svg";
 import UC from "./UC.svg";
+
+import AI from "./AI.svg";
+import CT from "./CT.svg";
+import EB from "./EB.svg";
+import LH from "./LH.svg";
+import MB from "./MB.svg";
+import MD from "./MD.svg";
+import PP from "./PP.svg";
+import SS from "./SS.svg";
+import ET from "./ET.svg";
+import IR from "./IR.svg";
 
 export const departmentSVGList = [
   {
@@ -31,12 +42,12 @@ export const departmentSVGList = [
     icon: GS,
   },
   {
-    name: "MA",
-    icon: MA,
+    name: "MC",
+    icon: MC,
   },
   {
-    name: "ME",
-    icon: ME,
+    name: "MA",
+    icon: MA,
   },
   {
     name: "MM",
@@ -49,5 +60,45 @@ export const departmentSVGList = [
   {
     name: "UC",
     icon: UC,
+  },
+  {
+    name: "AI",
+    icon: AI,
+  },
+  {
+    name: "CT",
+    icon: CT,
+  },
+  {
+    name: "EB",
+    icon: EB,
+  },
+  {
+    name: "LH",
+    icon: LH,
+  },
+  {
+    name: "MB",
+    icon: MB,
+  },
+  {
+    name: "MD",
+    icon: MD,
+  },
+  {
+    name: "PP",
+    icon: PP,
+  },
+  {
+    name: "SS",
+    icon: SS,
+  },
+  {
+    name: "ET",
+    icon: ET,
+  },
+  {
+    name: "IR",
+    icon: IR,
   },
 ];
--- src/constants/departmentColors.ts
+++
@@ -1,8 +0,0 @@
-import { theme } from "@/style/theme";
-
-export const departmentColors = {
-  major: theme.colors.inputBg,
-  minor: theme.colors.inputBg,
-  underGraduate: "#00b102",
-  default: "#E0E0E0",
-};
