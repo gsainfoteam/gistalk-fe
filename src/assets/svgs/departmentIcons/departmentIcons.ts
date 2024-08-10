4c34f77aa2c40046c71b0c2363f885e105808957
yeong <80435616+SeolyeongBae@users.noreply.github.com>
2024-08-10 20:43:20 +0900

    feat: icon 추가

---
+++ src/assets/svgs/departmentIcons/departmentIcons.ts
@@ -0,0 +1,53 @@
+import BS from "./BS.svg";
+import CH from "./CH.svg";
+import EC from "./EC.svg";
+import EV from "./EV.svg";
+import GS from "./GS.svg";
+import MA from "./MA.svg";
+import ME from "./ME.svg";
+import MM from "./MM.svg";
+import PS from "./PS.svg";
+import UC from "./UC.svg";
+
+export const departmentSVGList = [
+  {
+    name: "BS",
+    icon: BS,
+  },
+  {
+    name: "CH",
+    icon: CH,
+  },
+  {
+    name: "EC",
+    icon: EC,
+  },
+  {
+    name: "EV",
+    icon: EV,
+  },
+  {
+    name: "GS",
+    icon: GS,
+  },
+  {
+    name: "MA",
+    icon: MA,
+  },
+  {
+    name: "ME",
+    icon: ME,
+  },
+  {
+    name: "MM",
+    icon: MM,
+  },
+  {
+    name: "PS",
+    icon: PS,
+  },
+  {
+    name: "UC",
+    icon: UC,
+  },
+];
