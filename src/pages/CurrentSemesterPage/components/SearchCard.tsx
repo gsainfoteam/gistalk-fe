009815d7b23e1a63a585d1e2c9ea678ccb636d1e
yeong <80435616+SeolyeongBae@users.noreply.github.com>
2024-08-16 21:46:44 +0900

    build error resolved

--- src/pages/CurrentSemesterPage/components/SearchCard.tsx
+++ src/pages/CurrentSemesterPage/components/SearchCard.tsx
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 import { theme } from "@/style/theme";
-import TempIcon from "@/pages/SearchPage/components/TempIcon";
 import { major, minor, underG } from "@/constants/StdSet";
-import { departmentColors } from "@/constants/departmentColors";
 import { convertLectureCodeToList } from "@/utils";
 import useSubjectCode from "@/hooks/useSubjectCode";
 import { LectureCode } from "@/Interfaces/interfaces";
+import TempIcon from "./TempIcon";
 
 interface IProps {
   subjectCode: LectureCode[];
@@ -70,20 +69,13 @@ export default function SearchCard({
 
   const division = targetLectureCode.slice(0, 2);
 
-  /**major, minor 강의 코드와 대조해서 일치하면 전공/부전공/공통 과목 색상 부여 */
-  const IconColor: string = major
-    .map((item) => division === item.subjectCode)
-    .includes(true)
-    ? departmentColors.major
-    : minor.map((item) => division === item.subjectCode).includes(true)
-    ? departmentColors.minor
-    : underG.map((item) => division === item.subjectCode).includes(true)
-    ? departmentColors.underGraduate
-    : "#E0E0E0";
-
   return (
     <SearchCardWrap hoverColor={theme.colors.inputBg}>
-      <TempIcon text={division} color={IconColor} isChecked={false} />
+      <TempIcon
+        text={division}
+        color={theme.colors["gray-50"]}
+        isChecked={false}
+      />
       <CardContentWrap color={theme.colors.primaryText}>
         <div>{subjectName}</div>
         {/* 과목 이름 */}
