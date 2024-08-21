50c825193ba327226d14ef766065901ce33c1aba
hansemin <han299792@gmail.com>
2024-08-21 17:14:28 +0900

    chore: 불필요한 코드 삭제

--- src/pages/WriteReviewPage/components/modal.tsx
+++ src/pages/WriteReviewPage/components/modal.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 interface IProps {
